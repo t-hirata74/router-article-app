7cd1304fbb5430b74c39619e055b96e8fa605b80
t-hirata74 <t.hirata.0463@gmail.com>
2025-05-08 23:31:12 +0900

    人気記事一覧ページの実装

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,8 +1,36 @@
-export default function Home() {
+import type { Route } from "./+types/home";
+import { Article, type ArticleJson } from "~/domain/Article";
+
+export async function loader({ params }: Route.LoaderArgs) {
+  const res = await fetch(`https://qiita.com/api/v2/authenticated_user/items`, {
+    headers: {
+      Authorization: `Bearer ${import.meta.env.VITE_QIITA_API_KEY}`,
+    },
+  });
+  const articlesJson: ArticleJson[] = await res.json();
+  const articles = articlesJson.map(
+    (articleJson) =>
+      new Article(
+        articleJson.title,
+        articleJson.url,
+        articleJson.likes_count,
+        articleJson.stocks_count,
+        articleJson.created_at
+      )
+  );
+
+  return { articles };
+}
+
+export default function Home({ loaderData }: Route.ComponentProps) {
+  const { articles } = loaderData;
   return (
-    <div>
-      <div className="flex-1 sm:ml-64">
-        <h1>記事一覧</h1>
+    <div className="flex-1 sm:ml-64">
+      <h1>記事一覧</h1>
+      <div className="container mx-auto px-4 py-8">
+        {articles.map((article) => (
+          <p key={article.url}>{article.title}</p>
+        ))}
       </div>
     </div>
   );
--- app/routes/popular.tsx
+++ app/routes/popular.tsx
@@ -1,8 +1,39 @@
-export default function Popular() {
+import type { Route } from "./+types/popular";
+import { Article, type ArticleJson } from "~/domain/Article";
+
+export async function loader({ params }: Route.LoaderArgs) {
+  const res = await fetch(
+    `https://qiita.com/api/v2/items?page=1&per_page=20&query=user%3ASicut_study`,
+    {
+      headers: {
+        Authorization: `Bearer ${import.meta.env.VITE_QIITA_API_KEY}`,
+      },
+    }
+  );
+  const articlesJson: ArticleJson[] = await res.json();
+  const articles = articlesJson.map(
+    (articleJson) =>
+      new Article(
+        articleJson.title,
+        articleJson.url,
+        articleJson.likes_count,
+        articleJson.stocks_count,
+        articleJson.created_at
+      )
+  );
+
+  return { articles };
+}
+
+export default function Popular({ loaderData }: Route.ComponentProps) {
+  const { articles } = loaderData;
   return (
-    <div>
-      <div className="flex-1 sm:ml-64">
-        <h1>人気記事</h1>
+    <div className="flex-1 sm:ml-64">
+      <h1>人気記事一覧</h1>
+      <div className="container mx-auto px-4 py-8">
+        {articles.map((article) => (
+          <p key={article.url}>{article.title}</p>
+        ))}
       </div>
     </div>
   );
